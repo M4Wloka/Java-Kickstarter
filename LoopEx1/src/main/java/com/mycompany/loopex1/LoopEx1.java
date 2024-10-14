7ce6491fd9e916a9a03f4aa99646ca96a90f0fa7
Martyna Wloka <wlok4.mar@gmail.com>
2024-10-14 20:58:23 +0100

    Loops bulk exercises with java

---
+++ LoopEx1/src/main/java/com/mycompany/loopex1/LoopEx1.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loopex1;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Martyna
+ */
+
+public class LoopEx1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //1. Write a program to print all natural numbers from 1 to n
+    //        int a=10;
+    //        for(int b=1; b<=a; b++){
+    //            System.out.println(b);}
+//        2. Write a program to print all natural numbers in reverse
+    //        int n1 = 10;
+    //        int n2=20;
+    //        
+    //        for(int i =n2;i>=n1; i-- ){
+    //            System.out.println(i);
+//        
+//        }
+//        3. Write a program to print tables
+//                int number=2;
+//                int n=10;
+//                for(int i=1;i<=n;i++){
+//                System.out.println(number +"*"+ i+"="+(number*i));
+//                }
+//        4. Write a program to print reverse tables
+//                int number=2;
+//                int n=1;
+//                for(int i=10; i>=n; i--){
+//                System.out.println(number +"*"+ i+"="+(number*i));
+//                }
+//        5. Write a program to print all alphabets from a to z
+
+//            for(char a='a'; a<='z'; a++)
+//                System.out.println(a);
+
+        //6. Write a program to print reverse alphabets from Z to A
+        //        for(char b='A'; b<='Z'; b++){
+        //            System.out.println(b);
+        //        }
+//        7. Write a program to print all even numbers between 1 to 100
+//                int n=2;
+//                for(int i=0;i<=100;i++ ){
+//                if ((i%2)==0){
+//                System.out.println(i);
+//                }
+//                }
+//    8. Write a program to print all odd number between 1 to 100
+
+//                for(int i=0; i<=100;i++)
+//                    if((i%2)==1){
+//                    System.out.println(i);}
+//                    }
+
+//9. Write a program to find sum of all natural numbers between 1 to n
+
+//    int n=5;
+//    if (n<0){
+//    System.out.println("This is not natural number");
+//    }
+//    else if(n==0){
+//    System.out.println("Sum of 1+0=1");}
+//    else{
+//        double total=0;
+//        n=10;
+//        for(int i=1; i<=n; i++){
+//        total=(i*(i+1))/2;
+//        }
+//        System.out.println(total);   
+//        }
+//10. Write a program to find sum of all even numbers between 1 to n
+        
+    
+    }
+    
+}
