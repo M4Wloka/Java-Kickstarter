575a991a7ed1522b9495c3762e7ab341157ef8c8
Martyna Wloka <wlok4.mar@gmail.com>
2024-10-14 22:46:45 +0100

    Checking variables

---
+++ VariableApp/src/main/java/com/mycompany/variableapp/VariableApp.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variableapp;
+
+/**
+ *
+ * @author Martyna
+ */
+public class VariableApp {
+
+    public static void main(String[] args) {
+        int a; a=1;
+        long b; b=222;
+        short c; c=2;
+        float d; d=2.24f;
+        double e; e=1.111;
+        
+        System.out.println("Integer a is: " + a);
+        System.out.println("Long b is: " + b);
+        System.out.println("Short c is: " + c);
+        System.out.println("Float f is: "+ d);
+        System.out.println("Double e is: "+ e);
+        System.out.println("Addition: " + a + "+" + d + "=" + (a+d) );
+        System.out.println("Subtraction: " + b + "-" + c + "=" + (a-c));
+        System.out.println("Multiplication: " + c + "*" + d + "=" + (c*d));
+        System.out.println("Division: " + b +"/"+ c + "=" + (b/c));
+        
+        
+        
+        
+    }
+}
