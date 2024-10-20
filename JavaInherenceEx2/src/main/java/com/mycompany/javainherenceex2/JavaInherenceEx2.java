eb6c392b0ecb542e255cf69bb2745b63ebeecf57
Martyna Wloka <wlok4.mar@gmail.com>
2024-10-20 18:19:47 +0100

    OOP

---
+++ JavaInherenceEx2/src/main/java/com/mycompany/javainherenceex2/JavaInherenceEx2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javainherenceex2;
+
+/**
+ *
+ * @author Martyna
+ */
+public class JavaInherenceEx2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
