3265fe9354fd1a5cfcb3c72a67e173e68dc91d17
Martyna Wloka <wlok4.mar@gmail.com>
2024-10-21 19:59:56 +0100

    OOP

---
+++ JavaInherenceEx3/src/main/java/com/mycompany/javainherenceex3/Employee.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javainherenceex3;
+
+/**
+ *
+ * @author Martyna
+ */
+public class Employee {
+    
+    public double work(){
+    return 0;
+    }
+    public double getSalary(){
+    return 0;}
+    
+}
---
+++ JavaInherenceEx3/src/main/java/com/mycompany/javainherenceex3/JavaInherenceEx3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javainherenceex3;
+
+/**
+ *
+ * @author Martyna
+ */
+
+//Write a Java program to create a class called Employee with methods called work() and getSalary(). Create a subclass called HRManager that overrides the work() method and adds a new method called addEmployee().
+public class JavaInherenceEx3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
