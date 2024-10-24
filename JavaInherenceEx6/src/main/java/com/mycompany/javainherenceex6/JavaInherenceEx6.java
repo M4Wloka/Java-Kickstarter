f512c5824b93c22da258b98826bca9419165f2fa
Martyna Wloka <wlok4.mar@gmail.com>
2024-10-24 22:10:28 +0100

    OOP Inherence

---
+++ JavaInherenceEx6/src/main/java/com/mycompany/javainherenceex6/JavaInherenceEx6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javainherenceex6;
+
+/**
+ *
+ * @author Martyna
+ */
+public class JavaInherenceEx6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
