ed53d00b97d77a0d2b59178071e8a18f6c255ca4
Martyna Wloka <wlok4.mar@gmail.com>
2024-10-24 22:10:36 +0100

    OOP Inherence

---
+++ JavaInherenceEx7/src/main/java/com/mycompany/javainherenceex7/JavaInherenceEx7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javainherenceex7;
+
+/**
+ *
+ * @author Martyna
+ */
+public class JavaInherenceEx7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
